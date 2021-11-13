b0975fad0e8aaa069191b4e18505f01915ed37fc
nurliaaa <nur.lia.tif20@polban.ac.id>
2021-11-13 16:42:22 +0700

    fe

--- Working Product/FRONT-END/src/pages/index.js
+++ Working Product/FRONT-END/src/pages/index.js
@@ -1,43 +1,23 @@
 import SplashScreen from './SplashScreen';
-import SignUp from './SignUp';
-import SignUpForm from './SignUpForm';
 import CheckEmailToken from './CheckEmailToken';
 import CheckEmailForgot from './CheckEmailForgot';
 import CreateNewPassword from './CreateNewPassword';
 import SuccessCreatePassword from './SuccessCreatePassword';
-import SuccessSignUp from './SuccessSignUp';
 import SignIn from './SignIn';
 import ForgotPassword from './ForgotPassword';
 import WelcomeAuth from './WelcomeAuth';
-import Feed from './Feed';
-import Search from './Search';
-import Cycling from './Cycling';
-import SafetyTips from './SafetyTips';
-import SafetyVideo from './SafetyVideo';
-import SafetySertifikasi from './SafetySertifikasi';
 import Profile from './Profile';
 import ProfileMenus from './ProfileMenus';
-import TipsPDF from './TipsPDF';
 
 export {
-  SignUp,
   SignIn,
   WelcomeAuth,
-  SignUpForm,
   CheckEmailToken,
-  SuccessSignUp,
   ForgotPassword,
   SplashScreen,
   CheckEmailForgot,
   CreateNewPassword,
   SuccessCreatePassword,
-  Feed,
-  Search,
-  Cycling,
-  SafetyTips,
-  SafetyVideo,
-  SafetySertifikasi,
   Profile,
   ProfileMenus,
-  TipsPDF,
 };
--- Working Product/FRONT-END/src/router/index.js
+++ Working Product/FRONT-END/src/router/index.js
@@ -5,88 +5,78 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   SplashScreen,
   WelcomeAuth,
-  SignUp,
-  SignUpForm,
   CheckEmailToken,
-  SuccessSignUp,
   SignIn,
   ForgotPassword,
   CheckEmailForgot,
   SuccessCreatePassword,
   CreateNewPassword,
-  Feed,
-  Search,
-  Cycling,
-  SafetyTips,
-  SafetyVideo,
-  SafetySertifikasi,
   Profile,
   ProfileMenus,
-  TipsPDF,
 } from '../pages';
 import {BottomNavigator} from '../components';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+//const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator
-      sceneContainerStyle={{height: 1000}}
-      tabBar={(props) => <BottomNavigator {...props} />}
-      initialRouteName="Feed">
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Cycling" component={Cycling} />
-      <Tab.Screen name="Safety" component={SafetyStackScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
-  );
+    // <Tab.Navigator
+    //   sceneContainerStyle={{height: 1000}}
+    //   tabBar={(props) => <BottomNavigator {...props} />}
+    //   initialRouteName="Feed">
+    //   <Tab.Screen name="Feed" component={Feed} />
+    //   <Tab.Screen name="Search" component={Search} />
+    //   <Tab.Screen name="Cycling" component={Cycling} />
+    //   <Tab.Screen name="Safety" component={SafetyStackScreen} />
+    //   <Tab.Screen name="Profile" component={ProfileScreen} />
+    // </Tab.Navigator>
+  )
 };
 
-const ProfileScreen = () => {
-  return (
-    <Stack.Navigator initialRouteName="ProfileActivity">
-      <Stack.Screen
-        name="ProfileActivity"
-        component={Profile}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Stack.Screen
-        name="ProfileMenus"
-        component={ProfileMenus}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-    </Stack.Navigator>
-  );
-};
+// const ProfileScreen = () => {
+//   return (
+//     <Stack.Navigator initialRouteName="ProfileActivity">
+//       <Stack.Screen
+//         name="ProfileActivity"
+//         component={Profile}
+//         options={{headerShown: false, animationEnabled: false}}
+//       />
+//       <Stack.Screen
+//         name="ProfileMenus"
+//         component={ProfileMenus}
+//         options={{headerShown: false, animationEnabled: false}}
+//       />
+//     </Stack.Navigator>
+//   );
+// };
 
-const SafetyStackScreen = () => {
-  return (
-    <Stack.Navigator initialRouteName="SafetyTips">
-      <Stack.Screen
-        name="SafetyTips"
-        component={SafetyTips}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Stack.Screen
-        name="SafetyVideo"
-        component={SafetyVideo}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Stack.Screen
-        name="SafetySertifikasi"
-        component={SafetySertifikasi}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Stack.Screen
-        name="TipsPDF"
-        component={TipsPDF}
-        options={{headerShown: false}}
-      />
-    </Stack.Navigator>
-  );
-};
+// const SafetyStackScreen = () => {
+//   return (
+//     <Stack.Navigator initialRouteName="SafetyTips">
+//       <Stack.Screen
+//         name="SafetyTips"
+//         component={SafetyTips}
+//         options={{headerShown: false, animationEnabled: false}}
+//       />
+//       <Stack.Screen
+//         name="SafetyVideo"
+//         component={SafetyVideo}
+//         options={{headerShown: false, animationEnabled: false}}
+//       />
+//       <Stack.Screen
+//         name="SafetySertifikasi"
+//         component={SafetySertifikasi}
+//         options={{headerShown: false, animationEnabled: false}}
+//       />
+//       <Stack.Screen
+//         name="TipsPDF"
+//         component={TipsPDF}
+//         options={{headerShown: false}}
+//       />
+//     </Stack.Navigator>
+//   );
+// };
 
 const Router = () => {
   return (
@@ -106,7 +96,7 @@ const Router = () => {
         component={SignIn}
         options={{headerShown: false}}
       />
-      <Stack.Screen
+      {/* <Stack.Screen
         name="SignUp"
         component={SignUp}
         options={{headerShown: false}}
@@ -115,17 +105,17 @@ const Router = () => {
         name="SignUpForm"
         component={SignUpForm}
         options={{headerShown: false}}
-      />
+      /> */}
       <Stack.Screen
         name="CheckEmailToken"
         component={CheckEmailToken}
         options={{headerShown: false}}
       />
-      <Stack.Screen
+      {/* <Stack.Screen
         name="SuccessSignUp"
         component={SuccessSignUp}
         options={{headerShown: false}}
-      />
+      /> */}
       <Stack.Screen
         name="ForgotPassword"
         component={ForgotPassword}
